fe308068c0507fc1a59504268a4f63f5b82e6bed
Karol Działowski <karlososhd@gmail.com>
2020-10-22 20:09:14 +0200

    :sparkles: comparison of knn trees

--- lab2/generating_data.py
+++ lab2/generating_data.py
@@ -23,7 +23,7 @@ def generate_data(n_dimension=2, n_samples=100, noise=False):
     labels = np.sum(coeffs * samples[:, 0:-1], axis=1) + b > samples[:, -1]
     return samples, labels, (b, coeffs)
 
-def generate_checkerboard(n_size, n_samples):
+def generate_checkerboard(n_size, n_samples, noise=False):
     """Generate checkerboard of size n_size x n_size"""
     x = np.random.uniform(-(n_size//2)*np.pi, (n_size//2)*np.pi, size=(n_samples, 2))
     mask = np.logical_or(np.logical_and(sin(x[:, 0]) > 0.0, sin(x[:, 1]) > 0.0),
